62975a36e4d96de1915ee2ffa7b40956f9125082
Umutcan Çetin <66206623+NeuralG@users.noreply.github.com>
2024-01-19 00:14:46 +0300

    Hided the database for security.

--- filebase.js
+++ filebase.js
@@ -1,105 +1,101 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getStorage, uploadBytes, ref, listAll, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBBsj7Gcn6RrstjZixvG8vmscL0-XLl_yk",
-    authDomain: "safesoundboarddb.firebaseapp.com",
-    projectId: "safesoundboarddb",
-    storageBucket: "safesoundboarddb.appspot.com",
-    messagingSenderId: "697854367548",
-    appId: "1:697854367548:web:1cf1b0002e8db4e89f7039",
-    measurementId: "G-8086LZVZH3"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
+import {
+	getStorage,
+	uploadBytes,
+	ref,
+	listAll,
+	getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js"
+import { firebaseConfig } from "./firebaseConfig"
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getStorage(app);
+const app = initializeApp(firebaseConfig)
+const db = getStorage(app)
 
-const form = document.getElementById("form");
-const mainGrid = document.getElementById("main-grid");
+const form = document.getElementById("form")
+const mainGrid = document.getElementById("main-grid")
 
-let currentlyPlayingAudio = null;
+let currentlyPlayingAudio = null
 
 // Function to fetch existing sound files and create elements
 async function fetchAndCreateSoundElements() {
-  const storageRef = ref(db);
+	const storageRef = ref(db)
 
-  // List all items in the storage
-  const storageItems = await listAll(storageRef);
+	// List all items in the storage
+	const storageItems = await listAll(storageRef)
 
-  // Loop through the items and create elements for each sound
-  for (const item of storageItems.items) {
-    const downloadURL = await getDownloadURL(item);
-    const audioPlayer = createAudioPlayer(downloadURL, item.name);
+	// Loop through the items and create elements for each sound
+	for (const item of storageItems.items) {
+		const downloadURL = await getDownloadURL(item)
+		const audioPlayer = createAudioPlayer(downloadURL, item.name)
 
-    // Add play button
-    const playButton = document.createElement("button");
-    playButton.textContent = `${item.name}`;
-    playButton.addEventListener("click", () => togglePlay(audioPlayer));
+		// Add play button
+		const playButton = document.createElement("button")
+		playButton.textContent = `${item.name}`
+		playButton.addEventListener("click", () => togglePlay(audioPlayer))
 
-    // Add elements to the grid
-    mainGrid.appendChild(playButton);
-    mainGrid.appendChild(audioPlayer);
-  }
+		// Add elements to the grid
+		mainGrid.appendChild(playButton)
+		mainGrid.appendChild(audioPlayer)
+	}
 }
 
 // Fetch and create sound elements when the page loads
-fetchAndCreateSoundElements();
+fetchAndCreateSoundElements()
 
 form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const formData = new FormData(form);
-  const file = formData.get("file");
-  const name = formData.get("name");
+	e.preventDefault()
+	const formData = new FormData(form)
+	const file = formData.get("file")
+	const name = formData.get("name")
 
-  // Check if the file is an audio file
-  if (file && file.type.startsWith("audio/")) {
-    const fileRef = ref(db, name);
+	// Check if the file is an audio file
+	if (file && file.type.startsWith("audio/")) {
+		const fileRef = ref(db, name)
 
-    await uploadBytes(fileRef, file);
-    console.log(`File ${name} uploaded successfully`);
+		await uploadBytes(fileRef, file)
+		console.log(`File ${name} uploaded successfully`)
 
-    // Update the sound player with the new file URL
-    const downloadURL = await getDownloadURL(fileRef);
-    const audioPlayer = createAudioPlayer(downloadURL, name);
+		// Update the sound player with the new file URL
+		const downloadURL = await getDownloadURL(fileRef)
+		const audioPlayer = createAudioPlayer(downloadURL, name)
 
-    // Add play button
-    const playButton = document.createElement("button");
-    playButton.textContent = `${name}`;
-    playButton.addEventListener("click", () => togglePlay(audioPlayer));
+		// Add play button
+		const playButton = document.createElement("button")
+		playButton.textContent = `${name}`
+		playButton.addEventListener("click", () => togglePlay(audioPlayer))
 
-    // Add elements to the grid
-    mainGrid.appendChild(playButton);
-    mainGrid.appendChild(audioPlayer);
-  } else {
-    console.log("Invalid file type. Please upload an audio file.");
-  }
-});
+		// Add elements to the grid
+		mainGrid.appendChild(playButton)
+		mainGrid.appendChild(audioPlayer)
+	} else {
+		console.log("Invalid file type. Please upload an audio file.")
+	}
+})
 
 // Function to play or stop the sound
 function togglePlay(audioPlayer) {
-  if (currentlyPlayingAudio && currentlyPlayingAudio !== audioPlayer) {
-    // Stop the currently playing audio
-    currentlyPlayingAudio.pause();
-    currentlyPlayingAudio.currentTime = 0;
-  }
+	if (currentlyPlayingAudio && currentlyPlayingAudio !== audioPlayer) {
+		// Stop the currently playing audio
+		currentlyPlayingAudio.pause()
+		currentlyPlayingAudio.currentTime = 0
+	}
 
-  if (audioPlayer.paused) {
-    audioPlayer.play();
-    currentlyPlayingAudio = audioPlayer;
-  } else {
-    audioPlayer.pause();
-    audioPlayer.currentTime = 0;
-    currentlyPlayingAudio = null;
-  }
+	if (audioPlayer.paused) {
+		audioPlayer.play()
+		currentlyPlayingAudio = audioPlayer
+	} else {
+		audioPlayer.pause()
+		audioPlayer.currentTime = 0
+		currentlyPlayingAudio = null
+	}
 }
 
 // Function to create an audio player
 function createAudioPlayer(src, id) {
-  const audioPlayer = document.createElement("audio");
-  audioPlayer.src = src;
-  audioPlayer.id = `audio-player-${id}`;
-  audioPlayer.controls = true;
-  return audioPlayer;
+	const audioPlayer = document.createElement("audio")
+	audioPlayer.src = src
+	audioPlayer.id = `audio-player-${id}`
+	audioPlayer.controls = true
+	return audioPlayer
 }
